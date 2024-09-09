88f89954faaaeb3b4631895547c0ff1cf10c9e6f
Ezequiel <85073571+Ezequielp19@users.noreply.github.com>
2024-09-09 03:36:21 -0300

    Update evento.js

--- models/evento.js
+++ models/evento.js
@@ -8,6 +8,8 @@ const eventoSchema = new mongoose.Schema({
   titulo: { type: String},
   fecha: { type: Date},
   descripcion: { type: String }
+  categorias: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Categoria' }] // Referencia a la categoría
+
 });
 
 module.exports = mongoose.model('Evento', eventoSchema);
